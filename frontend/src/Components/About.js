8817d023860072c07608c383bda012391c1ed42f
Funmi <113897254+Amiele2006@users.noreply.github.com>
2023-12-16 17:51:00 +0100

    Add files via upload

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -1,4 +1,3 @@
-import image from './abtimg.jpg'
 import '../Stylesheets/About.css'
 import logo from './logo.png'
 import { useEffect, useContext } from 'react'
@@ -14,33 +13,22 @@ const About = () => {
 
   return (
     <div className='About'>
-      <div className='sideimage'>
-        <img src={image} className='images' alt='logo'/>
         <div className='textbox'>
           <h1 className={!dark ? 'story' : 'dstory'}>Our Mission</h1>
           <p className={!dark ? 'para1' : 'dpara1'}>
-            ist paragraph ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
+            AfriPulseHub aims to efficiently connect Donors with blood banks, recipients and hospitals while ensuring data
+            safety between the donor and the recipient.
           </p>
           <p className={!dark ? 'para2' : 'dpara2'}>
-            2nd paragraph ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-          </p>
-          <p className={!dark ? 'para3' : 'dpara3'}>
-            3rd paragraph ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
+          We want to ensure data safety between the donor and the recipient with timely and accessible blood supply. In a nutshell,
+          we aim to streamline the blood donation process as well as to foster a culture of regular donation and contributionto saving
+          lives in emergent situations.
           </p>
 
           <h1 className={!dark ? 'hours' : 'dhours'}>Our Values</h1>
-          <div className='sameline'><h2 className={!dark ? 'day1' : 'dday1'}>Val1:</h2><h2 className={!dark ? 'day11' : 'dday11'}>3rd paragraph ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem </h2></div>
-          <div className='sameline'><h2 className={!dark ? 'day1' : 'dday1'}>Val2:</h2><h2 className={!dark ? 'day2' : 'dday2'}>3rd paragraph ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem </h2></div>
-          <div className='sameline'><h2 className={!dark ? 'day1' : 'dday1'}>Val3:</h2><h2 className={!dark ? 'day3' : 'dday3'}>3rd paragraph ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsumlorem ipsum lorem ipsum lorem ipsum
-            lorem ipsum lorem </h2></div>
+          <div className='sameline'><h2 className={!dark ? 'day1' : 'dday1'}>Safety:</h2><h2 className={!dark ? 'day11' : 'dday11'}>Ensuring the safety of both the donors and the recipients through screenings an testing procedures</h2></div>
+          <div className='sameline'><h2 className={!dark ? 'day1' : 'dday1'}>Accessibility:</h2><h2 className={!dark ? 'day2' : 'dday2'}>Making blood products available to those in need, irrespective of their background, race or values</h2></div>
+          <div className='sameline'><h2 className={!dark ? 'day1' : 'dday1'}>Transparency:</h2><h2 className={!dark ? 'day3' : 'dday3'}>Being open and transparent about the processes, procedures and outcomes</h2></div>
           
 
           <h1 className={!dark ? 'clientelle' : 'dclientelle'}>Our sponsors</h1>
@@ -81,8 +69,6 @@ const About = () => {
         </ul>
         </div>
       </div>
-
-    </div>
   )
 }
 
